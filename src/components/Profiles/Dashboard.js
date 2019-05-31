82d54b4fccea491d38ade1b7d63e8b1fcc921881
Your Name <you@example.com>
2019-05-31 09:22:30 -0700

    final commit

--- src/components/Profiles/Dashboard.js
+++ src/components/Profiles/Dashboard.js
@@ -14,7 +14,7 @@ export default class Dashboard extends Component{
         super(props);
 
         this.state = {
-           comic:[],
+           comic:"",
            cloudinaryUrl: [],
            uploaded: '',
            edit: false
